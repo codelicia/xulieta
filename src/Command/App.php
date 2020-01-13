348d5cc25148341d984e9d981d45fc59dc16f6f5
Jefersson Nathan <malukenho.dev@gmail.com>
2020-01-13 14:51:06 +0100

    Clean-up App entry-point
    Reviewed-by: Alexandre Eher <alexandre.eher@werkspot.nl>
    Signed-off-by: Jefersson Nathan <malukenho.dev@gmail.com>

--- src/Command/App.php
+++ src/Command/App.php
@@ -14,6 +14,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Throwable;
 use function preg_match;
+use function sprintf;
 use const PHP_EOL;
 
 final class App extends Command
@@ -46,37 +47,16 @@ final class App extends Command
 
         $output->writeln("\nFinding documentation files on <info>" . $directory . "</info>\n");
 
-        $phpParser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
-
         /* @var $file \Symfony\Component\Finder\SplFileInfo */
         foreach ($finder as $file) {
-        $rst = new RstDocumentationFormat();
+            $rst = new RstDocumentationFormat();
 
-        if ($rst->canHandler($file)) {
-            $rst($file, $output);
-        } else {
-            echo "could not handle file: " . $file->getFilename();
-        }
-
-        //            try {
-//                foreach ($documentation->getNodes() as $node) {
-//                    if ($node instanceof CodeNode && $node->getLanguage() === 'php') {
-//
-//                        // FIXME: missing open php tag
-//                        if (! preg_match('/\<\?php/i', $node->getValueString())) {
-//                            $output->writeln('<error>Snippet missing PHP open tag on file: ' . $file->getRealPath() . '</error>');
-//                            continue;
-//                        }
-//                        $phpParser->parse($node->getValueString());
-//                    }
-//                }
-//            } catch (Throwable $e) {
-//
-//                $this->signalizeError();
-//
-//                $output->writeln('<error>Wrong code on file: ' . $file->getRealPath() . '</error>');
-//                $output->writeln($e->getMessage() . PHP_EOL);
-//            }
+            if ($rst->canHandler($file)) {
+                $rst($file, $output);
+            } else {
+                $output->writeln(sprintf('<error>Could not handle file "%s"</error>', $file->getFilename()));
+                $this->signalizeError();
+            }
         }
 
         if ($this->errorOccurred) {
