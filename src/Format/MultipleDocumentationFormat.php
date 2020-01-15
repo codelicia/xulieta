61d59c678a2b342379f3f49594526be56bd023e6
Jefersson Nathan <malukenho.dev@gmail.com>
2020-01-15 15:04:03 +0100

    Implements a chain of responsability to doc format
    Reviewed-by: Alexandre Eher <alexandre.eher@werkspot.nl>
    Signed-off-by: Jefersson Nathan <malukenho.dev@gmail.com>

---
+++ src/Format/MultipleDocumentationFormat.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Codelicia\Xulieta\Format;
+
+use Assert\Assert;
+use Symfony\Component\Console\Output\Output;
+use Symfony\Component\Finder\SplFileInfo;
+
+final class MultipleDocumentationFormat implements DocumentationFormat
+{
+    /** @var DocumentationFormat[] */
+    private array $documentationFormats;
+
+    public function __construct(DocumentationFormat ...$documentationFormats)
+    {
+        Assert::that($documentationFormats)
+            ->notEmpty();
+
+        $this->documentationFormats = $documentationFormats;
+    }
+
+    public function canHandler(SplFileInfo $file) : bool
+    {
+        foreach ($this->documentationFormats as $documentationFormat) {
+            if ($documentationFormat->canHandler($file)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function __invoke(SplFileInfo $file, Output $output) : bool
+    {
+        foreach ($this->documentationFormats as $documentationFormat) {
+            if ($documentationFormat->canHandler($file)) {
+                return $documentationFormat($file, $output);
+            }
+        }
+
+        return false;
+    }
+}
