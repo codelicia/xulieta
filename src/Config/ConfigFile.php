3434eca5266059a08f7f6e96f81391c5629505d6
Jefersson Nathan <malukenho.dev@gmail.com>
2020-02-02 07:55:19 +0100

    Parse configuration file
    Signed-off-by: Jefersson Nathan <malukenho.dev@gmail.com>

---
+++ src/Config/ConfigFile.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Codelicia\Xulieta\Config;
+
+use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\Yaml\Yaml;
+use function array_filter;
+use function current;
+use function file_get_contents;
+use function realpath;
+
+final class ConfigFile
+{
+    /**
+     * @param string $dir
+     *
+     * @return string[]
+     */
+    public static function loadInDirectory(string $dir) : array
+    {
+        $configFiles = current(array_filter(
+            [
+                $dir . '/../../../xulieta.yaml',
+                $dir . '/../../xulieta.yaml',
+                $dir . '/../xulieta.yaml',
+                $dir . '/xulieta.yaml',
+            ],
+            'is_file'
+        ));
+
+        $config = [];
+        if ($configFiles !== false) {
+            $config = (array) Yaml::parse(file_get_contents(realpath($configFiles)));
+        }
+
+        return (new Processor())
+            ->processConfiguration(new ConfigFileValidation(), $config);
+    }
+}
