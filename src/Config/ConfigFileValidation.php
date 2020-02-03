951f85f84530779cfcd454ba67f6ba41dbd38a33
Jefersson Nathan <malukenho.dev@gmail.com>
2020-02-03 07:56:00 +0100

    Add config validation for configuration file
    Signed-off-by: Jefersson Nathan <malukenho.dev@gmail.com>

---
+++ src/Config/ConfigFileValidation.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Codelicia\Xulieta\Config;
+
+use Codelicia\Xulieta\Format\MarkdownDocumentationFormat;
+use Codelicia\Xulieta\Format\RstDocumentationFormat;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+final class ConfigFileValidation implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder('xulieta');
+        $rootNode = $treeBuilder->getRootNode();
+
+        $rootNode
+            ->children()
+                ->arrayNode('plugins')
+                    ->addDefaultChildrenIfNoneSet()
+                    ->scalarPrototype()
+                        ->defaultValue([
+                            MarkdownDocumentationFormat::class,
+                            RstDocumentationFormat::class,
+                        ])
+                    ->end()
+                ->end()
+                ->arrayNode('exclude_dirs')
+                    ->addDefaultChildrenIfNoneSet()
+                    ->scalarPrototype()
+                        ->defaultValue(['vendor', 'node_modules'])
+                    ->end()
+                ->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
