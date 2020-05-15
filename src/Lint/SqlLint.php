6bcaf9deece85ac7d46f8a94b4118267af5630b7
Jefersson Nathan <malukenho.dev@gmail.com>
2020-05-15 16:24:27 +0200

    Do things better, faster, stronger (http://whatthecommit.com)
    Signed-off-by: Jefersson Nathan <malukenho.dev@gmail.com>

--- src/Lint/SqlLint.php
+++ src/Lint/SqlLint.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace Codelicia\Xulieta\Lint;
 
+use Exception;
 use PhpMyAdmin\SqlParser\Lexer;
 use PhpMyAdmin\SqlParser\Parser;
+use function array_map;
 use function count;
+use function implode;
+use const PHP_EOL;
 
 final class SqlLint implements Lint
 {
@@ -21,6 +25,13 @@ final class SqlLint implements Lint
 
     public function getViolation(string $code) : string
     {
-        return '';
+        $tokens = Lexer::getTokens($code, true);
+        $parser = new Parser($tokens);
+        $parser->parse();
+
+        return implode(PHP_EOL, array_map(
+            fn (Exception $e) => $e->getMessage(),
+            $parser->errors
+        ));
     }
 }
--- tests/unit/Lint/SqlLintTest.php
+++ tests/unit/Lint/SqlLintTest.php
@@ -13,34 +13,48 @@ final class SqlLintTest extends TestCase
      * @test
      * @dataProvider violationProvider
      */
-    public function itShouldDetectViolationsOnSql(bool $shouldHaveViolation, string $sql) : void
-    {
+    public function itShouldDetectViolationsOnSql(
+        string $sql,
+        bool $shouldHaveViolation,
+        string $violationList
+    ) : void {
         $subjectUnderTest = new SqlLint();
         self::assertEquals($shouldHaveViolation, $subjectUnderTest->hasViolation($sql));
+        self::assertEquals($violationList, $subjectUnderTest->getViolation($sql));
     }
 
     public function violationProvider() : array
     {
         return [
             'empty' => [
-                'shouldHaveViolation' => false,
                 'sql' => '',
+                'shouldHaveViolation' => false,
+                'violationList' => '',
             ],
             'simple select' => [
-                'shouldHaveViolation' => false,
                 'sql' => 'select * from test',
+                'shouldHaveViolation' => false,
+                'violationList' => '',
             ],
             'select missing fields' => [
-                'shouldHaveViolation' => true,
                 'sql' => 'select from test',
+                'shouldHaveViolation' => true,
+                'violationList' => 'An expression was expected.',
+            ],
+            'select with missing close parenthesis' => [
+                'sql' => 'SELECT , FROM User',
+                'shouldHaveViolation' => true,
+                'violationList' => "An expression was expected.\nUnexpected token.",
             ],
             'missing select' => [
-                'shouldHaveViolation' => true,
                 'sql' => 'from test',
+                'shouldHaveViolation' => true,
+                'violationList' => 'Unrecognized statement type.',
             ],
             'not a query' => [
-                'shouldHaveViolation' => true,
                 'sql' => 'banana is not a ;query',
+                'shouldHaveViolation' => true,
+                'violationList' => "Unexpected beginning of statement.\nUnrecognized statement type.",
             ],
         ];
     }
