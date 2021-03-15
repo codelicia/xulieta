d1dba8cea0d67905d86f77a8390c83f9c1405090
Viktor Szépe <viktor@szepe.net>
2021-03-15 21:01:59 +0100

    Improve ConfigFile

--- src/Config/ConfigFile.php
+++ src/Config/ConfigFile.php
@@ -27,15 +27,17 @@ final class ConfigFile
             'is_file'
         ));
 
-        $config = [];
-        if ($configFiles !== false) {
-            $config = (array) XmlUtils::convertDomElementToArray(
+        $config = $configFiles === false
+            ? []
+            : (array) XmlUtils::convertDomElementToArray(
                 XmlUtils::loadFile(realpath($configFiles))
                     ->documentElement
             );
-        }
 
         return (new Processor())
-            ->processConfiguration(new ConfigFileValidation(), ['xulieta' => $config]);
+            ->processConfiguration(
+                new ConfigFileValidation(),
+                ['xulieta' => $config]
+            );
     }
 }
