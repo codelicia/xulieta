cd02926d3bc11655e7e394bc051aa9fa5d5a4521
Jefersson Nathan <malukenho.dev@gmail.com>
2023-06-04 20:58:33 +0200

    chore(cs): ./vendor/bin/phpcbf
    Signed-off-by: Jefersson Nathan <malukenho.dev@gmail.com>

--- tests/Functional/init.php
+++ tests/Functional/init.php
@@ -2,8 +2,11 @@
 
 declare(strict_types=1);
 
+use function Psl\Env\current_dir;
+use function Psl\Str\format;
+
 (static fn () => require __DIR__ . '/../../vendor/autoload.php')();
 
 return static function (string $params): void {
-    system(Psl\Str\format('php %s/bin/xulieta check:erromeu %s', Psl\Env\current_dir(), $params));
+    system(format('php %s/bin/xulieta check:erromeu %s', current_dir(), $params));
 };
